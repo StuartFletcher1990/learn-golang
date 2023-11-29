485b0f68ea9591d183ddd69bb99acd753ce37d25
StuartFletcher1990 <stuart.fletcher@iceland.co.uk>
2023-11-29 14:59:40 +0000

    l

---
+++ export/export.go
@@ -0,0 +1,7 @@
+package export
+
+import "fmt"
+
+func ExportedFunction()  {
+	fmt.Println("im from another file!")
+}
--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	td "learn-golang/export"
+)
 
 func main()  {
 	fmt.Println("hello there!")
+	//importing goes off the folder name
+	td.ExportedFunction()
 }
